66bf6645ce276689cc6b5d6c6e80d70d3c9e330e
loveandsheep <sheep@backspace.tokyo>
2021-12-24 14:18:34 +0900

    goto修正

--- src/motorTrack.h
+++ src/motorTrack.h
@@ -64,7 +64,7 @@ public:
             {
                 logHost = m.getRemoteHost();
                 lastLog = "Go to " + ofToString(m.getArgAsInt(1));
-                motor.goAbs(motorIndex, m.getArgAsInt(1), 300 / stepDeg, 1000 / stepDeg, 1000 / stepDeg);
+                motor.goAbs(motorIndex, m.getArgAsInt(1) / stepDeg, 300 / stepDeg, 1000 / stepDeg, 1000 / stepDeg);
             }
 
             if (m.getAddress() == "/track/preset")
